1f5804f83a662dc8474fdf0ee4edb9b0cd22e47c
yosef <yousef@gamil.com>
2026-05-18 02:57:38 +0300

    show

--- try.cpp
+++ try.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 string name = "ahmed";
 float money = 99.9;
+ void show();
+ void minu( int a);
 
 int main()
 {
-	
+	void minu( int a);
+	 show();
 	return 0;
+}
+void show(){
+	cout<<"your account name is :"<<name<<endl;
+		cout<<"you have( "<<money<<" ) in your account"<<endl;
+	
+}
+void minu( int a){
+	cout<<"Choose the thing you need :"<<endl;
+	cout<<" 1- to deposit monet\n 2- to take money\n 3- to show your accounts"<<endl;
+	cin>>a;
 }
